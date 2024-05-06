5c49ede32b957a2a4156f2bb1534cf0fd0d8f6bd
Jake0925 <jakehk20@gmail.com>
2024-05-06 10:42:53 +0800

    Cmake분할
    dependency cmake 생성함

--- lecture_1/main.cpp
+++ lecture_1/main.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
 
-int main(int argc, const char** argv)
-{
-    std::cout<<"hello, opengl"<< std::endl;
-
+int main(int argc, const char** argv) {
+    SPDLOG_INFO("Hello, test!");
     return 0;
 }
+
+// #include <iostream>
+
+// int main(int argc, const char** argv)
+// {
+//     std::cout<<"hello, opengl"<< std::endl;
+
+//     return 0;
+// }
+
+
