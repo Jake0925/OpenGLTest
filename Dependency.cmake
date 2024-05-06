15a52a6d59be8b4d73ec7d43f38307d2456432b3
Jake0925 <jakehk20@gmail.com>
2024-05-06 22:41:24 +0800

    GLFW 창만들기
    창만들기

---
+++ Dependency.cmake
@@ -0,0 +1,42 @@
+
+# 외부라이브러리 사용하기 --------------------------------------------------------------
+# ExternalProject 관련 명령어 셋 추가
+include(ExternalProject) #include는 cmake에서 추가적인 기능을 사용할때 사용하는 명령
+
+# Dependercy 관련 변수 설정
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
+set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
+set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
+
+# spdlog: fast logger library 라이브러리 사용
+# GIT_SHALLOW: 0 - GIT_SHALLOW를 비활성화  1- 활성화,  GIT_SHALLOW는 가장 최신의 코드만 다운받을수 있게한다
+ExternalProject_Add(
+    dep_spdlog
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" TEST_COMMAND "" PATCH_COMMAND ""
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+    )
+
+# Dependency 리스트 및 라이브러리 파일 리스트 추가
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
+set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+
+
+
+# glfw ----------------------------
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.3"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" PATCH_COMMAND "" TEST_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+    )
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
